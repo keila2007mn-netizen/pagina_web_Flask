17cbf1ca56f2754c97b23f139c0002b7e6628056
Keila <keila2007@gmail.com>
2025-09-04 12:24:34 -0600

    Actualizar proyecto con imágenes, CSS y JS

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Ruta principal de la página web
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(host='0.0.0.0', port=5000)
